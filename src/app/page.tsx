87c85b55fa6c8e68f3005a3942e4ac3710da32b3
Higor Araujo <higorribeiroaraujo007@gmail.com>
2025-07-08 08:46:52 -0300

    feat: finalização do card de viagem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import { AppProvider, type Navigation } from '@toolpad/core/AppProvider';
 import { DashboardLayout } from '@toolpad/core/DashboardLayout';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import { FaArrowLeft } from 'react-icons/fa'
 
 const NAVIGATION: Navigation = [
   {
@@ -35,7 +34,7 @@ const demoTheme = createTheme({
 
 export default function DashboardLayoutFullScreen() {
   useEffect(() => {
-    var map = L.map('map').setView([-0.5947849, -47.3178818], 14);
+    const map = L.map('map').setView([-0.5947849, -47.3178818], 14);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -43,10 +42,10 @@ export default function DashboardLayoutFullScreen() {
 
     const customIcon = L.divIcon({
       html: `
-        <div class="bg-white rounded-lg shadow-md h-full">
+        <div class="bg-white rounded-lg shadow-md h-full hover:scale-110 transition-transform duration-500">
           <div class="card-marker-content">
             <img 
-              style="width: 250px; height: 150px; object-fit: cover; border-radius: 4px 4px 0 0;" 
+              style="width: 300px; height: 150px; object-fit: cover; border-radius: 4px 4px 0 0;" 
               src="https://agencia-api-rest.onrender.com/images/1751835542930_10746.jpg" 
               alt="Local"
             />
@@ -56,17 +55,29 @@ export default function DashboardLayoutFullScreen() {
                 <span class="text-black rounded px-2">1/1</span>
                 <button class="text-primary text-[16px] font-semibold">&gt;</button>
               </div>
-              <div class="flex flex-col gap-1] p-1">
-                <h3 class="text-primary text-[16px] font-bold">Salinópolis</h3>
-                <p class="text-primary text-[12px] text-justify font-normal">Viagem para a praia de Salinas, hospedagem em hotel 5 estrelas (Hotel Concha do Mar), com direito a 2 cafés da manhã.</p>
+              <div class="flex flex-col gap-1 p-1">
+                <h3 class="text-primary text-[16px] font-bold w-[284px] truncate">Salinópolis</h3>
+                <p class="text-primary text-[12px] text-justify font-normal h-[54px] line-clamp-3">Viagem para a praia de Salinas, hospedagem em hotel 5 estrelas (Hotel Concha do Mar), com direito a 2 cafés da manhã.</p>
                 <span>⭐⭐⭐⭐⭐</span>
+                <div class="flex flex-row!important gap-1 justify-between mt-1">
+                  <div class="bg-green-300 shadow-md rounded p-1 text-green-700 text-center font-semibold w-1/2">
+                    01/08/2025
+                  </div>
+                  <div class="bg-green-300 shadow-md rounded p-1 text-green-700 text-center font-semibold w-1/2">
+                    50 vagas
+                  </div>
+                </div>
+                <span class="my-2 text-[22px] text-black">R$ 600,00</span>
+                <button class="rounded border-[0.5px] border-primary border-solid bg-white text-primary font-medium p-2 hover:bg-primary hover:text-white transition-all duration-500">
+                  SABER MAIS
+                </button>
               </div>
             </div>
           </div>
         </div>
       `,
       className: 'custom-html-marker',
-      iconSize: [250, 350]
+      iconSize: [300, 430]
     });
 
     L.marker([-0.5947849, -47.3178818], { icon: customIcon }).addTo(map);
